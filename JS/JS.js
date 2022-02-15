5c7b3c32fad5322509cfd0fc1bc190b263801ba8
MOMaher891 <75702799+MOMaher891@users.noreply.github.com>
2022-02-15 13:12:29 +0200

    Add files via upload

--- JS/JS.js
+++ JS/JS.js
@@ -1,1 +1,21 @@
+document.querySelector('.fa-magnifying-glass').addEventListener('click', () => {
+    document.querySelector('.form-control').style.display = "block";
+})
 
+
+document.querySelector('.i-1').addEventListener('click', () => {
+    document.querySelector('.badge1').style.display = 'none';
+})
+document.querySelector('.i-2').addEventListener('click', () => {
+    document.querySelector('.badge2').style.display = 'none';
+})
+
+
+
+document.querySelector('.home').addEventListener('click', timedRefresh)
+
+function timedRefresh(timeoutPeriod) {
+    setTimeout("location.reload(true);", timeoutPeriod);
+}
+
+document.querySelector('.button').addEventListener('click', timedRefresh)
